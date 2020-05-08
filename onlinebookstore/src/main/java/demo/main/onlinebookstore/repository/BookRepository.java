dba8130933a095b0e7a5d14a390189d163dbb9be
yorabi <yorabi@LMA002087.corp.capgemini.com>
2020-05-08 10:11:29 +0000

    Authorized crossOrigin Localhost:4200

--- onlinebookstore/src/main/java/demo/main/onlinebookstore/repository/BookRepository.java
+++ onlinebookstore/src/main/java/demo/main/onlinebookstore/repository/BookRepository.java
@@ -1,9 +1,11 @@
 package demo.main.onlinebookstore.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import demo.main.onlinebookstore.entity.Book;
 
+@CrossOrigin("http://localhost:4200")
 public interface BookRepository extends JpaRepository<Book, Long>{
 
 }
